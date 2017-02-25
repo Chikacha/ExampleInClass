9731d109a3ea0f6b64407ff37e89a90c257ab5c7
Chikacha <geecoolggwp@gmail.com>
2017-02-24 22:09:45 -0800

    Example added

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,90 @@
+class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+
+  # GET /orders
+  # GET /orders.json
+  def index
+    #binding.pry
+    @orders = Order.all
+  end
+
+  # GET /orders/1
+  # GET /orders/1.json
+  def show
+    @comments = @order.comments
+    @comment = Comment.new
+  end
+
+  def vote
+    @order = Order.find(params[:id])    
+    @vote = Vote.create(voteable: @order, creator: current_user, vote: params[:vote])
+
+    redirect_to root_path
+  end
+
+  # GET /orders/new
+  def new
+    @order = Order.new
+  end
+
+  # GET /orders/1/edit
+  def edit
+  end
+
+  # POST /orders
+  # POST /orders.json
+  def create
+    # binding.pry
+    @order = Order.new(order_params)
+    
+    if @order.save
+      redirect_to @order
+    else
+      render :new
+    end
+    # respond_to do |format|
+    #   if @order.save
+    #     format.html { redirect_to @order, notice: 'Order was successfully created.' }
+    #     format.json { render :show, status: :created, location: @order }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @order.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
+
+  # PATCH/PUT /orders/1
+  # PATCH/PUT /orders/1.json
+  def update
+    respond_to do |format|
+      if @order.update(order_params)
+        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
+        format.json { render :show, status: :ok, location: @order }
+      else
+        format.html { render :edit }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /orders/1
+  # DELETE /orders/1.json
+  def destroy
+    @order.destroy
+    respond_to do |format|
+      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def order_params
+      params.require(:order).permit(:name, :phone, :description)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  root 'orders#index'
+
+  resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  resources :users, only: [:new, :create]
+  get '/register', to: 'users#new'
+  resources :orders do
+    member do # 客製化連結
+      post :vote
+      # 這樣會產出 orders/1/vote
+    end
+    # 嵌套/巢狀路由，這樣可以產生出 orders/1/comments/ 的 url
+    resources :comments, only: [:create] do
+      member do
+        post :vote
+        # 產出 orders/1/comments/1/vote
+      end
+    end
+  end
 end
